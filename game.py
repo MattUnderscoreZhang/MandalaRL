8584d551824111449b8ec9e420beea5336f13ace
Matt Zhang <set.stun@gmail.com>
2021-07-12 22:04:36 -0400

    Almost fully jitted but insanely slow version of the code

--- game.py
+++ game.py
@@ -1,12 +1,11 @@
-from my_types import ColorCount, CardOrdering, Key, Color, Zone
+from my_types import ColorCount, Key, Color, Zone, n_colors, n_zones
 from typing import NamedTuple
-from jax import random, jit, numpy as np
-from jax.ops import index, index_add, index_update
+from jax import random, jit, lax, numpy as np
+from functools import partial
 
 
 class Game(NamedTuple):
     state: ColorCount  # how many colors are in each zone
-    deck_ordering: CardOrdering  # ordered list of cards
     finished_deck_once: bool  # whether or not we've cycled through all the cards
 
 
@@ -14,55 +13,80 @@ def init_game(key: Key) -> Game:
     """
     Sets up a game.
     """
-    game_state = np.zeros((len(Zone), len(Color)))
-    game_state = index_update(game_state, index[Zone.DECK], np.array([18] * len(Color)))
+    # make new game with a fresh deck
+    game_state = np.zeros((n_zones, n_colors))
+    game_state = game_state.at[Zone.DECK].set(np.tile(18, n_colors))
+    game = Game(game_state, False)
     # put two cards in each cup
-    # place two cards in each mountain
-    deck_ordering = shuffle_deck(game_state[Zone.DECK], key)
-    game = Game(game_state, deck_ordering, False)
     key, key1, key2 = random.split(key, 3)
-    game = place_from_deck(game, Zone.P1_HAND, 6, key1)
-    game = place_from_deck(game, Zone.P2_HAND, 6, key2)
+    game = move_from_deck(game, Zone.P1_CUP, 2, key1)
+    game = move_from_deck(game, Zone.P2_CUP, 2, key2)
+    # place two cards in each mountain
+    key, key1, key2 = random.split(key, 3)
+    game = move_from_deck(game, Zone.M1_MOUNTAIN, 2, key1)
+    game = move_from_deck(game, Zone.M2_MOUNTAIN, 2, key2)
+    # each player draws 6 cards
+    key, key1, key2 = random.split(key, 3)
+    game = move_from_deck(game, Zone.P1_HAND, 6, key1)
+    game = move_from_deck(game, Zone.P2_HAND, 6, key2)
     return game
 
 
-def shuffle_deck(cards: ColorCount, key: Key) -> CardOrdering:
+@jit
+def move_discard_to_deck(game: Game) -> Game:
     """
-    Takes a ColorCount array and returns a randomized array of colors.
-    """
-    deck_ordering = [[color] * int(cards[color]) for color in Color]
-    deck_ordering = np.array([i for j in deck_ordering for i in j])
-    deck_ordering = random.shuffle(key, deck_ordering)
-    return deck_ordering
-
-
-def shuffle_discard_into_deck(game: Game, key: Key) -> Game:
-    """
-    Shuffle discard pile into deck.
+    Move discard pile into deck.
     """
     game_state = game.state
-    game_state = index_update(game_state, index[Zone.DECK], game_state[Zone.DISCARD])
-    game_state = index_update(game_state, index[Zone.DISCARD], np.zeros(len(Color)))
-    deck_ordering = shuffle_deck(game_state[Zone.DECK], key)
-    game = Game(game_state, deck_ordering, True)
+    game_state = game_state.at[Zone.DECK].set(game_state[Zone.DISCARD])
+    game_state = game_state.at[Zone.DISCARD].set(np.zeros(n_colors))
+    game = Game(game_state, True)
     return game
 
 
-def place_from_deck(game: Game, zone: Zone, n_cards: int, key: Key) -> Game:
+@jit
+def move_one_card_from_deck(zone: Zone, game: Game, key: Key) -> tuple[Game, None]:
+    """
+    Move one card from the deck to another zone.
+    """
+    game = lax.cond(np.sum(game.state[Zone.DECK]) == 0,
+                    lambda _: move_discard_to_deck(game),
+                    lambda _: game,
+                    None)
+    game_state = game.state
+    color = random.choice(key, n_colors, p=game.state[Zone.DECK])
+    game_state = game_state.at[Zone.DECK, color].add(-1)
+    game_state = game_state.at[zone, color].add(1)
+    return Game(game_state, game.finished_deck_once), None
+
+
+def move_from_deck(game: Game, zone: Zone, n_cards: int, key: Key) -> Game:
     """
     Move cards from the deck into another zone.
     """
+    keys = random.split(key, n_cards)
+    game, _ = lax.scan(partial(move_one_card_from_deck, zone), game, keys)
+    return game
+
+
+@jit
+def move_from_zone(game: Game, zone_1: Zone, zone_2: Zone, color: Color, n_cards: int) -> Game:
+    """
+    Move cards from one zone into another zone.
+    Does not check for legality of action.
+    """
     game_state = game.state
-    deck_ordering = game.deck_ordering
-    for _ in range(n_cards):
-        if len(deck_ordering) == 0:
-            key, key1 = random.split(key, 2)
-            game = Game(game_state, deck_ordering, game.finished_deck_once)
-            game = shuffle_discard_into_deck(game, key1)
-            game_state = game.state
-            deck_ordering = game.deck_ordering
-        color = deck_ordering[0]
-        game_state = index_add(game_state, index[zone, color], 1)
-        game_state = index_add(game_state, index[Zone.DECK, color], -1)
-        deck_ordering = deck_ordering[1:]
-    return Game(game_state, deck_ordering, game.finished_deck_once)
+    game_state = game_state.at[zone_1, color].add(-n_cards)
+    game_state = game_state.at[zone_2, color].add(n_cards)
+    return Game(game_state, game.finished_deck_once)
+
+
+@jit
+def move_all_from_zone(game: Game, zone_1: Zone, zone_2: Zone) -> Game:
+    """
+    Move all cards from one zone into another zone.
+    """
+    game_state = game.state
+    game_state = game_state.at[zone_2].set(game_state[zone_1] + game_state[zone_2])
+    game_state = game_state.at[zone_1].set(np.zeros(n_colors))
+    return Game(game_state, game.finished_deck_once)
--- test_game.py
+++ test_game.py
@@ -1,10 +1,9 @@
-from my_types import Color, Zone
+from my_types import Color, Zone, n_colors
 import unittest
 import game
 from jax import random, numpy as np
-from jax.random import PRNGKey
-from jax.ops import index, index_update
-
+from jax.random import PRNGKey 
+from jax.ops import index
 
 class GameTests(unittest.TestCase):
     def setUp(self):
@@ -14,66 +13,107 @@ class GameTests(unittest.TestCase):
         self.my_game = game.init_game(key1)
 
 
+    def assert_state_validity(self, state):
+        color_counts = np.sum(state, axis=0)
+        assert(np.sum(state) == 108)
+        assert(all(color_counts == np.tile(18, 6)))
+
+
     def test_enums(self):
         assert(len(Color) == 6)
         assert(len(Zone) == 14)
 
 
-    def test_shuffle(self):
-        self.key, key1 = random.split(self.key, 2)
-        deck_ordering = game.shuffle_deck(self.my_game.state[Zone.DECK], key1)
-        color_counts = np.sum(self.my_game.state, axis=0)
-        assert(len(deck_ordering) == 96)
-        assert(all(color_counts == np.array([18] * 6)))
-
-
-    def test_shuffle_discard_into_deck(self):
-        self.key, key1 = random.split(self.key, 2)
-        game_state = self.my_game.state
-        game_state = index_update(game_state, index[Zone.DISCARD], game_state[Zone.DECK])
-        game_state = index_update(game_state, index[Zone.DECK], np.zeros(len(Color)))
-        test_game = game.Game(game_state, np.array([]), self.my_game.finished_deck_once)
-        test_game = game.shuffle_discard_into_deck(test_game, key1)
-        color_counts = np.sum(test_game.state, axis=0)
-        assert(len(test_game.deck_ordering) == 96)
-        assert(all(color_counts == np.array([18] * 6)))
+    def test_move_discard_into_deck(self):
+        state = self.my_game.state
+        state = state.at[index[Zone.DISCARD]].set(state[Zone.DECK])
+        state = state.at[index[Zone.DECK]].set(np.zeros(n_colors))
+        test_game = game.Game(state, self.my_game.finished_deck_once)
+        test_game = game.move_discard_to_deck(test_game)
+        state = test_game.state
+        assert(np.sum(state[Zone.DECK]) == 88)
+        self.assert_state_validity(state)
 
 
     def test_draw(self):
         self.key, key1, key2 = random.split(self.key, 3)
         test_game = self.my_game
-        test_game = game.place_from_deck(test_game, Zone.P1_HAND, 10, key1)
-        test_game = game.place_from_deck(test_game, Zone.P2_HAND, 5, key2)
+        test_game = game.move_from_deck(test_game, Zone.P1_HAND, 10, key1)
+        test_game = game.move_from_deck(test_game, Zone.P2_HAND, 5, key2)
         state = test_game.state
-        deck_ordering = test_game.deck_ordering
-        assert(len(deck_ordering) == 81)
-        assert(np.sum(state[Zone.DECK]) == 81)
+        assert(np.sum(state[Zone.DECK]) == 73)
         assert(np.sum(state[Zone.P1_HAND]) == 16)
         assert(np.sum(state[Zone.P2_HAND]) == 11)
+        self.assert_state_validity(state)
 
 
     def test_draw_empty_deck(self):
         # place all but 30 cards from the deck into the discard pile
-        colors, counts = np.unique(self.my_game.deck_ordering, return_counts=True)
-        original_color_counts = [i[1] for i in sorted(zip(colors, counts))]
-        remaining_deck_ordering = self.my_game.deck_ordering[-30:]
-        colors, counts = np.unique(remaining_deck_ordering, return_counts=True)
-        deck_color_counts = [i[1] for i in sorted(zip(colors, counts))]
-        discard_color_counts = [i-j for i, j in zip(original_color_counts, deck_color_counts)]
-        game_state = self.my_game.state
-        game_state = index_update(game_state, index[Zone.DECK, :], np.array(deck_color_counts))
-        game_state = index_update(game_state, index[Zone.DISCARD, :], np.array(discard_color_counts))
-        test_game = game.Game(game_state, remaining_deck_ordering, self.my_game.finished_deck_once)
+        self.key, key1 = random.split(self.key, 2)
+        test_game = game.move_from_deck(self.my_game, Zone.DISCARD, 58, key1)
         # have each player draw 20 cards
         self.key, key1, key2 = random.split(self.key, 3)
-        test_game = game.place_from_deck(test_game, Zone.P1_HAND, 20, key1)
-        test_game = game.place_from_deck(test_game, Zone.P2_HAND, 20, key2)
+        test_game = game.move_from_deck(test_game, Zone.P1_HAND, 20, key1)
+        test_game = game.move_from_deck(test_game, Zone.P2_HAND, 20, key2)
         # check states
         state = test_game.state
-        deck_ordering = test_game.deck_ordering
-        assert(len(deck_ordering) == 56)
-        assert(np.sum(state[Zone.DECK]) == 56)
+        assert(np.sum(state[Zone.DECK]) == 48)
         assert(np.sum(state[Zone.DISCARD]) == 0)
         assert(np.sum(state[Zone.P1_HAND]) == 26)
         assert(np.sum(state[Zone.P2_HAND]) == 26)
-        assert(np.sum(state) == 108)
+        self.assert_state_validity(state)
+
+
+    def test_move_cards_in_cup(self):
+        # place 20 more cards into each cup
+        self.key, key1, key2 = random.split(self.key, 3)
+        test_game = self.my_game
+        test_game = game.move_from_deck(test_game, Zone.P1_CUP, 20, key1)
+        test_game = game.move_from_deck(test_game, Zone.P2_CUP, 20, key2)
+        # check states
+        state = test_game.state
+        assert(np.sum(state[Zone.DECK]) == 48)
+        assert(np.sum(state[Zone.DISCARD]) == 0)
+        assert(np.sum(state[Zone.P1_HAND]) == 6)
+        assert(np.sum(state[Zone.P2_HAND]) == 6)
+        assert(np.sum(state[Zone.P1_CUP]) == 22)
+        assert(np.sum(state[Zone.P2_CUP]) == 22)
+        self.assert_state_validity(state)
+
+
+    def test_move_cards_from_zone(self):
+        # place cards from hand onto Mandala
+        test_game = self.my_game
+        p1_colors, p1_counts = np.unique(test_game.state[Zone.P1_HAND], return_counts=True)
+        test_game = game.move_from_zone(test_game, Zone.P1_HAND, Zone.M1_MOUNTAIN, int(p1_colors[0]), int(p1_counts[0]))
+        p2_colors, p2_counts = np.unique(test_game.state[Zone.P2_HAND], return_counts=True)
+        test_game = game.move_from_zone(test_game, Zone.P2_HAND, Zone.M2_MOUNTAIN, int(p2_colors[0]), int(p2_counts[0]))
+        # check states
+        state = test_game.state
+        assert(np.sum(state[Zone.DECK]) == 88)
+        assert(np.sum(state[Zone.DISCARD]) == 0)
+        assert(np.sum(state[Zone.P1_HAND]) == 6-p1_counts[0])
+        assert(np.sum(state[Zone.P2_HAND]) == 6-p2_counts[0])
+        assert(np.sum(state[Zone.P1_CUP]) == 2)
+        assert(np.sum(state[Zone.P2_CUP]) == 2)
+        assert(np.sum(state[Zone.M1_MOUNTAIN]) == 2+p1_counts[0])
+        assert(np.sum(state[Zone.M2_MOUNTAIN]) == 2+p2_counts[0])
+        self.assert_state_validity(state)
+
+
+    def test_move_all_cards_from_zone(self):
+        # place all cards from hand onto Mandala
+        test_game = self.my_game
+        test_game = game.move_all_from_zone(test_game, Zone.P1_HAND, Zone.M1_MOUNTAIN)
+        test_game = game.move_all_from_zone(test_game, Zone.P2_HAND, Zone.M2_MOUNTAIN)
+        # check states
+        state = test_game.state
+        assert(np.sum(state[Zone.DECK]) == 88)
+        assert(np.sum(state[Zone.DISCARD]) == 0)
+        assert(np.sum(state[Zone.P1_HAND]) == 0)
+        assert(np.sum(state[Zone.P2_HAND]) == 0)
+        assert(np.sum(state[Zone.P1_CUP]) == 2)
+        assert(np.sum(state[Zone.P2_CUP]) == 2)
+        assert(np.sum(state[Zone.M1_MOUNTAIN]) == 8)
+        assert(np.sum(state[Zone.M2_MOUNTAIN]) == 8)
+        self.assert_state_validity(state)
